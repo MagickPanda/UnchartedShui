f15be9300ca36f201ca511a8092128c108bd4246
MagickPanda <35385424+MagickPanda@users.noreply.github.com>
2020-12-21 00:32:19 +0800

    Add files via upload
    *intial import

---
+++ UnchartedShuiServer/mainwindow.h
@@ -0,0 +1,37 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QSqlDatabase>
+#include <QMessageBox>
+
+class NetworkMod
+{
+public:
+    void readMessage(void);
+    void sendMessage(QString msg, QTcpSocket *socket);
+    void processMessage(QString msg,QTcpSocket *socket);
+};
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow, private NetworkMod
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    QTcpServer *tcpServer;
+    QTcpSocket *tcpSocket;
+};
+
+#endif // MAINWINDOW_H
